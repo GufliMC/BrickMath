5710ab27fd134ee624773edfac830f05d5156ef9
Joris Guffens <joris.guffens@outlook.com>
2022-07-12 20:04:32 +0200

    database converter stuff

--- database-api/src/main/java/com/guflimc/brick/maths/database/api/LocationConverter.java
+++ database-api/src/main/java/com/guflimc/brick/maths/database/api/LocationConverter.java
@@ -2,25 +2,25 @@ package com.guflimc.brick.maths.database.api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.guflimc.brick.maths.api.geo.Position;
+import com.guflimc.brick.maths.api.geo.Location;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import marcono1234.gson.recordadapter.RecordTypeAdapterFactory;
 
 @Converter
-public class PositionConverter implements AttributeConverter<Position, String> {
+public class LocationConverter implements AttributeConverter<Location, String> {
 
     private static final Gson gson = new GsonBuilder()
             .registerTypeAdapterFactory(RecordTypeAdapterFactory.DEFAULT)
             .create();
 
     @Override
-    public String convertToDatabaseColumn(Position attribute) {
+    public String convertToDatabaseColumn(Location attribute) {
         return gson.toJson(attribute);
     }
 
     @Override
-    public Position convertToEntityAttribute(String dbData) {
-        return gson.fromJson(dbData, Position.class);
+    public Location convertToEntityAttribute(String dbData) {
+        return gson.fromJson(dbData, Location.class);
     }
 }
